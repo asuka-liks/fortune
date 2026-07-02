1725998fc6490f6f23c16357ebe62af5ab396cea
asuka-liks <1286382298@qq.com>
2026-07-02 17:54:16 +0800

    feat: 添加公安备案号

--- components/layout/AppFooter.vue
+++ components/layout/AppFooter.vue
@@ -1,14 +1,25 @@
 <template>
   <footer class="border-t bg-white px-4 py-2 text-center text-xs text-gray-400">
     <p>{{ t('app.footer') }}</p>
-    <a
-      href="https://beian.miit.gov.cn/"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="hover:text-gray-500 transition-colors"
-    >
-      苏ICP备2026041456号-1
-    </a>
+    <div class="flex items-center justify-center gap-3 flex-wrap">
+      <a
+        href="https://beian.miit.gov.cn/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="hover:text-gray-500 transition-colors"
+      >
+        苏ICP备2026041456号-1
+      </a>
+      <a
+        href="https://beian.mps.gov.cn/#/query/webSearch?code=32021402004828"
+        target="_blank"
+        rel="noreferrer"
+        class="flex items-center gap-1 hover:text-gray-500 transition-colors"
+      >
+        <img src="/beian.png" alt="公安备案" class="w-3 h-3" />
+        苏公网安备32021402004828号
+      </a>
+    </div>
   </footer>
 </template>
 
